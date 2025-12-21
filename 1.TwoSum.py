7130356aeaf648d5ded19e1a04fa2be8acd40779
zee <235075031+snoozy-coder@users.noreply.github.com>
2025-12-21 10:49:42 +0700

    Add explanation

--- 1.TwoSum.py
+++ 1.TwoSum.py
@@ -1,8 +1,11 @@
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
+        hash_map = {}                                               #Tạo hashmap để lưu các phần tử đã gặp
 
-        for idx, num in enumerate(nums):
-            if target - num in hash_map:
-                return [idx,hash_map[target - num]]
-            hash_map[num] = idx 
+        for idx, num in enumerate(nums):                            #Duyệt idx và giá trị ứng với idx
+            if target - num in hash_map:                            #Nếu hiệu của target và phần tử hiện tại đã có trong hashmap
+                return [idx,hash_map[target - num]]                 #Trả ra idx của phần tử hiện tại và hiệu 
+            hash_map[num] = idx                                     #Nếu chưa có, lưu trong hashmap với key là phần tử, value là idx của phần tử đó
+                                                                    #Time: O(n) - Duyệt các phần tử trong mảng
+                                                                    #Space: O(n) - Tạo hashmap để lưu các phần tử đã xuất hiện
